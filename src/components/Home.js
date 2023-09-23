2e98b9a991d97269635b718775a76a519b10370c
simran <simrankumaribodhgaya93@gmail.com>
2023-09-23 15:43:37 +0530

    color changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React  from 'react'
 import leetcode from '../../src/images/leetcode2.png';
 import codeforces from '../../src/images/code-forces.png';
 import codechef from '../../src/images/icons8-codechef-48.png';
@@ -6,6 +6,9 @@ import hackerrank from '../../src/images/hackerrank.png';
 import geeksforgeeks from '../../src/images/icons8-geeksforgeeks-48.png';
 import w3schools from '../../src/images/w3schools.png';
 import code_calender from '../../src/images/code_calender.png';
+import topcoder from '../../src/images/topcoder.png';
+import codingninjas from '../../src/images/codingninja.png';
+import hackerearth from '../../src/images/hackerearth.223x256.png'
 import './Homes.css';
 import './Homes.css';
 //import Lists from './Lists'
@@ -14,58 +17,48 @@ import './Homes.css';
 
 
 const Home = () => {
+  const handleClick = () => {
+    window.location.replace('#');
+  };
   return (
      
     <>
-       <div className='head'><img src={code_calender}></img></div>
+       <div className='head'><img alt=""src={code_calender}></img></div>
     <div className='all'>
          <div class='wrap'>
               <div className='Sec'>
                 <div className='subSec'>
-                      <div className='op'><div className='in'><img src={leetcode}></img><span className='title'>LeetCode</span></div></div>
-                      <div className='op'><div className='in'><img src={codeforces}></img><span className='title'>CodeForces</span></div></div>
-                      <div className='op'><div className='in'><img src={codechef}></img><span className='title'>CodeChef</span></div></div>
+  <div className='op'><div className='in'><img alt="" src={leetcode}></img><span className='title' onClick={handleClick}>LeetCode</span></div></div>
+  <div className='op'><div className='in'><img alt=""src={codeforces}></img><span className='title' onClick={handleClick}>CodeForces</span></div></div>
+  <div className='op'><div className='in'><img alt=""src={codechef}></img><span className='title' onClick={handleClick}>CodeChef</span></div></div>
                       
                     
                  </div>
                  <div className='subSec'>
-                     <div className='op'><div className='in'><img src={hackerrank}></img><span className='title'>HackerRank</span></div></div>
-                      <div className='op'><div className='in'><img src={geeksforgeeks}></img><span className='title'>geeksforgeeks</span></div></div>
-                      <div className='op'><div className='in'><img src={w3schools}></img><span className='title'>w3schools</span></div></div>
+  <div className='op'><div className='in'><img alt="" src={hackerrank}></img><span className='title' onClick={handleClick}>HackerRank</span></div></div>
+  <div className='op'><div className='in'><img alt="" src={geeksforgeeks}></img><span className='title' onClick={handleClick}>geeksforgeeks</span></div></div>
+  <div className='op'><div className='in'><img alt="" src={w3schools}></img><span className='title' onClick={handleClick}>w3schools</span></div></div>
+                      
+                    
+                 </div>
+                 <div className='subSec'>
+  <div className='op'><div className='in'><img alt="" src={topcoder}></img><span className='title' onClick={handleClick}>TopCoder</span></div></div>
+  <div className='op'><div className='in'><img alt="" src={codingninjas}></img><span className='title' onClick={handleClick}>CodingNinjas</span></div></div>
+  <div className='op'><div className='in'><img alt="" src={hackerearth}></img><span className='title' onClick={handleClick}>HackerEarth</span></div></div>
                       
                     
                  </div>
 
                 </div>
-             <div className='up'><button>Upcoming Contests</button></div>
+             <div className='up'><button onClick={handleClick} >Upcoming Contests</button></div>
               
          </div>
          
     </div>
-  {/*<h1 align="center">Code Calender</h1>
-    <div className="container">
-     <div><img src="D:\Web Development\code_calender\public\code cal images\leetcode2.png" alt=""/>LeetCode</div><br/>
-     <div><img src="D:\Web Development\code_calender\public\code cal images\code-forces.png" alt=""/>Codeforces</div><br/>
-     <div><img src="D:\Web Development\code_calender\public\code cal images\codingninja.png" alt=""/>CodingNinjas</div><br/>
-     <div><img src="D:\Web Development\code_calender\public\code cal images\icons8-geeksforgeeks-48.png" alt=""/></div>Geeks for Geeks<br/>
-     <div><img src="D:\Web Development\code_calender\public\code cal images\icons8-codechef-48.png" alt=""/>CodeChef</div><br/>
-     <div><img src="D:\Web Development\code_calender\public\code cal images\topcoder.png" alt=""/>TopCoder</div><br/>
-     <div><img src="D:\Web Development\code_calender\public\code cal images\hackerrank.png" alt=""/>HackerRank</div><br/>
-     <div><img src="D:\Web Development\code_calender\public\code cal images\hackerearth.223x256.png" alt=""/>Hackerearth</div><br/>
-     </div>*/}
-     
+ 
 </>
   )
 }
    
 
 export default Home;
-
-
-/* </div>
-    <div>LeetCode</div>
-    <div><img src=""/>CodeForces</div>
-    <div><img src=""/>CodingNinjas</div>
-    <div><img src=""/>Geeks For Geeks</div>
-    <div><img src=""/>CodeChef</div>
-    <div><img src=""/>TopCoder</div> */
